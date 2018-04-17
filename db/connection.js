9bf0b8e5fddb801fada12e84e248ac006fec8f0c
Meron Kebede <kebedemeron905@gmail.com>
2018-04-17 15:31:56 -0400

    deploying app to heroku

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,11 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/bookshelv_db')
 mongoose.Promise = Promise
 
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(process.env.MLAB_URL)
+} else {
+  mongoose.connect('mongodb://localhost/bookshelv_db')
+}
 module.exports = mongoose
